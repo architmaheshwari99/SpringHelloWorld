54cd5c76b738ad56fd53509bf4fe0b7c37a759e6
maheshwari <archit.maheshwari@payufin.com>
2023-05-18 02:26:39 +0530

    Building a basic game app with tight coupling

---
+++ src/main/java/com/mahes/archit/learnspringframework/AppGamingBasicJava.java
@@ -0,0 +1,15 @@
+package com.mahes.archit.learnspringframework;
+
+public class AppGamingBasicJava {
+
+    public static void main(String[] args) {
+
+        var marioGame = new MarioGame();
+        var gameRunner = new GameRunner(marioGame);
+
+        gameRunner.run();
+
+
+    }
+
+}
---
+++ src/main/java/com/mahes/archit/learnspringframework/MarioGame.java
@@ -0,0 +1,21 @@
+package com.mahes.archit.learnspringframework;
+
+public class MarioGame {
+
+
+    public void up() {
+        System.out.println("Jump");
+    }
+
+    public void down() {
+        System.out.println("Sit");
+    }
+
+    public void left() {
+        System.out.println("Back");
+    }
+
+    public void right() {
+        System.out.println("Run");
+    }
+}
