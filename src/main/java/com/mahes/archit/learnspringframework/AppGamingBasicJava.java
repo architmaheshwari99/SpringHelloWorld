37a8c310dd0dd01d6d7f3cdf482ae7e6a81db28b
maheshwari <archit.maheshwari@payufin.com>
2023-05-18 02:32:05 +0530

    Evident tight coupling when new game is added

--- src/main/java/com/mahes/archit/learnspringframework/AppGamingBasicJava.java
+++ src/main/java/com/mahes/archit/learnspringframework/AppGamingBasicJava.java
@@ -5,6 +5,9 @@ public class AppGamingBasicJava {
     public static void main(String[] args) {
 
         var marioGame = new MarioGame();
+// We would need to change GameRunner constructor
+//  to run contraGame. This is tight coupling
+        var contraGame = new ContraGame();
         var gameRunner = new GameRunner(marioGame);
 
         gameRunner.run();
---
+++ src/main/java/com/mahes/archit/learnspringframework/ContraGame.java
@@ -0,0 +1,21 @@
+package com.mahes.archit.learnspringframework;
+
+public class ContraGame {
+
+    public void up() {
+        System.out.println("Jump");
+    }
+
+    public void down() {
+        System.out.println("Aim");
+    }
+
+    public void left() {
+        System.out.println("Run back");
+    }
+
+    public void right() {
+        System.out.println("Shoot");
+    }
+
+}
