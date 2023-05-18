c39849061dfebc5381b44b7539adbf512af916a5
maheshwari <archit.maheshwari@payufin.com>
2023-05-18 12:06:11 +0530

    using @Configuration class in main file and deleting other configuration file

--- src/main/java/com/mahes/archit/learnspringframework/App03GamingBasicJava.java
+++ src/main/java/com/mahes/archit/learnspringframework/App03GamingBasicJava.java
@@ -2,7 +2,25 @@ package com.mahes.archit.learnspringframework;
 
 import com.mahes.archit.learnspringframework.game.GameRunner;
 import com.mahes.archit.learnspringframework.game.GamingConsole;
+import com.mahes.archit.learnspringframework.game.MarioGame;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+class GameConfiguration {
+    @Bean
+    public GamingConsole game(){
+        return new MarioGame();
+    }
+
+    @Bean
+    public GameRunner runner(GamingConsole game){
+        return new GameRunner(game);
+    }
+}
+
 
 public class App03GamingBasicJava {
 
