91b6656b60f0e10fecfbb84af1f48e995f41a4ec
maheshwari <archit.maheshwari@payufin.com>
2023-05-18 12:44:00 +0530

    Setter Field Level Injection

--- src/main/java/com/mahes/archit/learnspringframework/a2/SimpleAppLauncherJava.java
+++ src/main/java/com/mahes/archit/learnspringframework/a2/SimpleAppLauncherJava.java
@@ -11,11 +11,20 @@ import java.util.Arrays;
 @Component
 class YourBusinessClass{
 
-    @Autowired
     Dependency1 d1;
 
-    @Autowired
     Dependency2 d2;
+
+
+    @Autowired
+    public void setD1(Dependency1 d1) {
+        this.d1 = d1;
+    }
+
+    @Autowired
+    public void setD2(Dependency2 d2) {
+        this.d2 = d2;
+    }
 
     @Override
     public String toString() {
