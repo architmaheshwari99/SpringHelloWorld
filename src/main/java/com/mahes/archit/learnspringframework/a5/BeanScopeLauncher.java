4339c8110865cfd2444c9143888d81ae46a90685
maheshwari <archit.maheshwari@payufin.com>
2023-05-19 12:42:35 +0530

    Prototype vs Singleton Bean Scopes

---
+++ src/main/java/com/mahes/archit/learnspringframework/a5/BeanScopeLauncher.java
@@ -0,0 +1,37 @@
+package com.mahes.archit.learnspringframework.a5;
+
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+class NormalClass {}
+
+
+@Scope(value= ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+@Component
+class PrototypeClass{}
+
+@Configuration
+@ComponentScan
+public class BeanScopeLauncher {
+
+
+
+    public static void main(String[] args) {
+
+        var context = new AnnotationConfigApplicationContext(BeanScopeLauncher.class);
+
+        System.out.println(context.getBean(NormalClass.class));
+        System.out.println(context.getBean(NormalClass.class));
+
+        System.out.println(context.getBean(PrototypeClass.class));
+        System.out.println(context.getBean(PrototypeClass.class));
+        System.out.println(context.getBean(PrototypeClass.class));
+
+    }
+
+}
